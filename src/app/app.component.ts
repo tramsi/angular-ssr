961622799bd202479e8e53099528123bc3e1c96a
Mohamed Altramsi <m.tramsi@ynmodata.com>
2023-12-18 19:22:29 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './library/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
+  buttonStyle: 'primary' | 'secondary' = 'primary';
 }
